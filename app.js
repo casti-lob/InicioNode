53c42520655b80a09f7ca7da34d628183fa95bd6
casti-lob <casti_lob@hotmail.com>
2023-04-11 10:58:30 +0200

    atlas

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { dbConnection } = require('./database/config.js');
 const app = express();
-
+//PARA QUE FUNCIONE EL .env
+require ('dotenv').config();
 const birds = require('./routers/birds.js')
 const guitars = require('./routers/guitars.js')
 
+
+async function connectAtlas(){
+    await dbConnection()
+}
+connectAtlas()
+console.log(process.env.PRUEBA)
 //middleware
 app.use(express.json())
 
@@ -22,4 +30,5 @@ app.use('guitars/add',guitars)
 app.use('guitars/:id',guitars)
 
 
-app.listen(3000)
+app.listen(process.env.PORT)
+console.log(`Server listening on port${process.env.PORT}`)
